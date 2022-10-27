5d2ae83202109e7a623b91673b1ddbcadd9db4cc
Andrew Liaw <yhaliaw@example.com>
2022-10-27 10:02:50 +0800

    Update copyright notice date.

--- keyboards/planck/keymaps/yhaliaw/keymap.c
+++ keyboards/planck/keymaps/yhaliaw/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2020 Andrew Liaw
+/* Copyright 2022 Andrew Liaw
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
