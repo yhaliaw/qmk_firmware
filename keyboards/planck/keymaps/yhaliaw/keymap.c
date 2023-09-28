f01b3658ef960d3262b3e4eafa66a4ebcd43ed62
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-09-28 17:34:15 +0800

    Swap space and enter on yhaliaw planck keymap

--- keyboards/planck/keymaps/yhaliaw/keymap.c
+++ keyboards/planck/keymaps/yhaliaw/keymap.c
@@ -43,14 +43,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |Leader| Ctrl |  Alt |  GUI | Lower| Enter| Space| Raise|  GUI |  Alt | Ctrl |  Del |
+     * |Leader| Ctrl |  Alt |  GUI | Lower| Space| Enter| Raise|  GUI |  Alt | Ctrl |  Del |
      * `-----------------------------------------------------------------------------------'
      */
     [_BASE] = LAYOUT_planck_grid(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        QK_LEAD, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_ENT,  KC_SPC,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_DEL
+        QK_LEAD, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RGUI, KC_RALT, KC_RCTL, KC_DEL
     ),
 
     /* Lower
